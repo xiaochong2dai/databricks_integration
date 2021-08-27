2465652ac163fd9cd73cd060c8e835872472442a
xiaochong1218@gmail.com <xiaochong1218@gmail.com>
2021-08-27 00:27:08 +0000

    test for git integration

---
+++ test_git.py
@@ -0,0 +1,48 @@
+# Databricks notebook source
+a = 1
+
+# COMMAND ----------
+
+a
+
+# COMMAND ----------
+
+spark
+
+# COMMAND ----------
+
+from pyspark.sql.types import StructType, StructField, StringType, ArrayType, IntegerType
+
+df = spark.createDataFrame(
+    [
+        ('Alice', ['math101', 'ee101', 'cs201'], 'Apple, Orange, Pineapple', ['red', 'blue'], 19, 'F', 'CS'), 
+        ('Bob', ['ee101', 'math102'], 'Peach, Banana', ['green', 'red'], 22, 'M', 'EE'), 
+        ('Jason', ['math101'], 'Stawberry, Blueberry', ['purple'], 20, 'M', 'MATH')
+    ], 
+    schema=StructType([
+        StructField('name', StringType(), False),
+        StructField('classes', ArrayType(StringType()), True),
+        StructField('fruits', StringType()),
+        StructField('favorite_colors', ArrayType(StringType(), False)),
+        StructField('age', IntegerType()),
+        StructField('gender', StringType()),
+        StructField('major', StringType())
+    ])
+)
+df.createOrReplaceTempView('_df')
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC SELECT
+# MAGIC   name,
+# MAGIC   age
+# MAGIC FROM _df
+
+# COMMAND ----------
+
+df.saveAsTable('test')
+
+# COMMAND ----------
+
+
